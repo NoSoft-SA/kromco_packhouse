26eba0194e43c4231ba3187058a1eaa270e1726e
jmt1 <p.netsai@gmail.com>
2015-10-14 12:20:38 +0200

    order products query changed

--- lib/mes_scada/grid_plugins/fg/order_product_grid_plugin.rb
+++ lib/mes_scada/grid_plugins/fg/order_product_grid_plugin.rb
@@ -8,8 +8,7 @@ module MesScada::GridPlugins
       end
 
       def calc_queries
-        @pallets=Pallet.find_by_sql("select p.id,op.id as order_product_id from pallets p
-                                   join load_details ld on p.load_detail_id=ld.id
+        @load_details=LoadDetail.find_by_sql("select ld.id,op.id as order_product_id from load_details ld
                                    join order_products op on ld.order_product_id=op.id
                                    where op.order_id=#{@env.session.data[:active_doc]['order']}")
       end
@@ -33,10 +32,10 @@ module MesScada::GridPlugins
           cell_value= subtotal
         end
         if column_name=="delete"
-          pallets=@pallets.find_all { |p| p.order_product_id.to_i==record.id.to_i }
+          load_details=@load_details.find_all { |p| p.order_product_id.to_i==record.id.to_i }
 
-          cell_value= make_action("http://#{@request.host_with_port}/"+"fg/order_product/delete_order_product" + "/" + record['id'].to_s,'delete')  if pallets.empty?
-          cell_value= nil if !pallets.empty?
+          cell_value= make_action("http://#{@request.host_with_port}/"+"fg/order_product/delete_order_product" + "/" + record['id'].to_s,'delete')  if load_details.empty?
+          cell_value= nil if !load_details.empty?
         end
         cell_value
       end
