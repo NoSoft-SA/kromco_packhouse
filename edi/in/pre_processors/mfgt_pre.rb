e32b589467e6e63bcb1753a419ca466491ac4cb9
hanszietsman <hansjuriezietsman@gmail.com>
2016-01-07 17:52:59 +0200

    Hans: added some pre-processors for fixed len master files so that record padding will work for such master files

---
+++ edi/in/pre_processors/mfgt_pre.rb
@@ -0,0 +1,12 @@
+class MfgtPre < RecordPadder
+
+
+  #override base class method
+   def required_record_length(flow_type, record_type, current_length, must_have_size=false)
+     required_record_length_for_mf(flow_type,record_type,current_length,must_have_size)
+   end
+
+
+
+end
+
