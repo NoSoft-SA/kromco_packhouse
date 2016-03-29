6d3a2b33aecdf47da6d81cf45f4c23ffcce2367e
luxolo <lmatoti@gmail.com>
2016-03-29 16:15:47 +0200

    working from home

---
+++ lib/mes_scada/grid_plugins/production/ppecb_extra_info_plugin.rb
@@ -0,0 +1,18 @@
+module MesScada::GridPlugins
+  module Production
+    class PpecbExtraInfoPlugin < MesScada::GridPlugin
+      def initialize(env = nil)
+        @env     = env
+      end
+
+      # def render_cell(column_name,cell_value,record)
+      #   if(column_name == "menu_item")
+      #     return cell_value +  "[#{@menu_items_friendly_names[record[:menu_item]]}]"
+      #   end
+      #
+      #   return "Blackson" #cell_value= @env.text_field('ppecb_inspection', 'inspection_point', {:size=>30,:value=>record[column_name]})
+      # end
+
+    end
+  end
+end
