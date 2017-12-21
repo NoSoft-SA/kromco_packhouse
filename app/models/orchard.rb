e2404560bb158855a5245e6269e010db48d625ef
user <lmatoti@gmail.com>
2017-12-21 11:36:21 +0200

    Latest

--- app/models/orchard.rb
+++ app/models/orchard.rb
@@ -1,33 +1,32 @@
 class Orchard < ActiveRecord::Base
-    
+
   belongs_to :farm
   belongs_to :representative_orchard, :class_name => 'Orchard', :foreign_key => 'parent_orchard_id'
 
   #MM102014-add virtual variable commodity id
   attr_accessor :orchard_commodity_id, :commodity_code#, :commodity_description_long
-#===================================
-#   Validations
-#===================================
+  #===================================
+  #   Validations
+  #===================================
   validates_presence_of :orchard_code
-  validates_presence_of :orchard_rmt_variety_id
   # validates_uniqueness_of :orchard_code
 
   def validate
-      if self.new_record?
-         validate_uniqueness
-      end
+    if self.new_record?
+      validate_uniqueness
+    end
   end
 
   def validate_uniqueness
-     exists = Orchard.find_by_farm_id_and_orchard_code(self.farm_id,self.orchard_code)
-     if exists != nil
-         errors.add_to_base("There already exists a record with the combined values of fields: 'orchard_code'")
-     end
+    exists = Orchard.find_by_farm_id_and_orchard_code(self.farm_id,self.orchard_code)
+    if exists != nil
+      errors.add_to_base("There already exists a record with the combined values of fields: 'orchard_code'")
+    end
   end
 
 
 
-  def integrate_representative_orchard_into_MAF
+  def after_create
     begin
       rmt = RmtVariety.find_by_sql("select commodity_code from rmt_varieties where rmt_varieties.id = #{self.orchard_rmt_variety_id}")
       if rmt[0].commodity_code=='AP'
@@ -50,40 +49,18 @@ class Orchard < ActiveRecord::Base
           raise error
         end
 
-        parcelles = ActiveRecord::Base.connection.select_all("select distinct farms.farm_code, orchard_code, rmt_varieties.rmt_variety_code, track_slms_indicator_code
+        parcelles = ActiveRecord::Base.connection.select_all("select distinct farms.farm_code, rmt_varieties.rmt_variety_code, track_slms_indicator_code, o.orchard_code
                                                   from orchards
                                                   inner join farms on farms.id =orchards.farm_id
                                                   inner join rmt_varieties on rmt_varieties.id = orchards.orchard_rmt_variety_id
                                                   inner join track_slms_indicators on track_slms_indicators.rmt_variety_code = rmt_varieties.rmt_variety_code
+                                                  left outer join orchards o on o.id=orchards.parent_orchard_id
                                                   where farms.id = #{self.farm_id}
                                                   and orchards.id = #{self.id}
                                                   and rmt_varieties.id = #{self.orchard_rmt_variety_id}  and track_indicator_type_code='RMI'
                                                   order by farms.farm_code ")
 
-        if(!parcelles.empty?)
-          existing_pacel_clause = "where Code_parcelle='#{parcelles.map{|p| "#{p.orchard_code}_#{p.farm_code}_#{p.track_slms_indicator_code}" }.join("' or Code_parcelle='")}' "
-          http = Net::HTTP.new(Globals.bin_created_mssql_server_host, Globals.bin_created_mssql_presort_server_port)
-          request = Net::HTTP::Post.new("/select")
-          parameters = {'method' => 'select', 'statement' => Base64.encode64("SELECT *
-            FROM [productionv50].[dbo].[Parcelle]
-            #{existing_pacel_clause}")}
-          request.set_form_data(parameters)
-          response = http.request(request)
-
-          if '200' == response.code
-            res = response.body.split('resultset>').last.split('</res').first
-            if(!Marshal.load(Base64.decode64(res)).empty?)
-              return
-            end
-
-          else
-            err = response.body.split('</message>').first.split('<message>').last
-            error = " \"\". The http code is #{response.code}. Message: #{err}."
-            raise error
-          end
-        end
-
-        inserts = []
+        inserts = ['BEGIN TRANSACTION ']
         parcelles.each do |parcelle|
           #-------------------------------------------------------------------------------------------------------------------------------------------
           #-------------------------------------------------------------------------------------------------------------------------------------------
@@ -110,20 +87,20 @@ class Orchard < ActiveRecord::Base
           #-------------------------------------------------------------------------------------------------------------------------------------------
           #-------------------------------------------------------------------------------------------------------------------------------------------
         end
+        inserts << ' COMMIT TRANSACTION'
 
-        if(!inserts.empty?)
-          http = Net::HTTP.new(Globals.bin_scanned_mssql_server_host, Globals.bin_created_mssql_presort_server_port)
-          request = Net::HTTP::Post.new("/exec")
-          parameters = {'method' => 'insert', 'statement' => Base64.encode64(inserts.join)}
-          request.set_form_data(parameters)
-          response = http.request(request)
+        http = Net::HTTP.new(Globals.bin_scanned_mssql_server_host, Globals.bin_created_mssql_presort_server_port)
+        request = Net::HTTP::Post.new("/exec")
+        parameters = {'method' => 'insert', 'statement' => Base64.encode64(inserts.join)}
+        request.set_form_data(parameters)
+        response = http.request(request)
 
-          if response.code != '200'
-            err = response.body.split('</message>').first.split('<message>').last
-            errmsg = " \"INSERT INTO [productionv50].[dbo].[Parcelle]\". The http code is #{response.code}. Message: #{err}."
-            raise errmsg
-          end
+        if response.code != '200'
+          err = response.body.split('</message>').first.split('<message>').last
+          errmsg = " \"INSERT INTO [productionv50].[dbo].[Parcelle]\". The http code is #{response.code}. Message: #{err}."
+          raise errmsg
         end
+
       end
     rescue
       raise "SQL MF Automatic Integration returned an error: #{$!.message}"
