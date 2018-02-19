8b9fd1d7cf0d0a979cd53de2252ca247d3ea31fe
user <lmatoti@gmail.com>
2018-02-19 14:39:43 +0200

    4] Deliveries: adding the rmt_product_id: disallow (or raise exception) adding ofrmt_product if 'intake_bin_scanning' is in progress (i.e. intake_bin_scanning.data_activated has a value, but date_completed is null)

--- app/models/pdt_transactions/receive_intake_bin/receive_intake_bin.rb
+++ app/models/pdt_transactions/receive_intake_bin/receive_intake_bin.rb
@@ -196,7 +196,7 @@ class ReceiveIntakeBin < PDTTransaction
 
     bin_rec                          = Bin.new
     bin_rec.bin_number               = bin_number
-    bin_rec.rmt_product_id           = 11278
+    bin_rec.rmt_product_id           = @delivery.rmt_product_id
     bin_rec.season_code              = @delivery.season_code
     bin_rec.delivery_id              = @delivery.id
     bin_rec.farm_id                  = @delivery.farm_id
@@ -213,7 +213,7 @@ class ReceiveIntakeBin < PDTTransaction
     bin_rec.track_indicator4_id = delivery_track[3].track_slms_indicator_id if  delivery_track[3]
     bin_rec.track_indicator5_id = delivery_track[4].track_slms_indicator_id if  delivery_track[4]
     bin_rec.save
-puts
+    return bin_rec
   end
 
   def complete_bin_scan_trans
@@ -223,26 +223,28 @@ puts
     begin
       ActiveRecord::Base.transaction do
         bin_nums=Array.new
+        bins = []
         self.full_bins.each { |b| bin_nums << b }
-
         self.half_bins.each { |c| bin_nums << c }
 
-
         for bin_number in bin_nums
-          create_bin(bin_number)
+          bins << create_bin(bin_number)
         end
-        #(owner_party_role_id,stock_type,farm_code,truck_code,trans_name,trans_id,location_code,stock_ids)
+
+        updates = []
+        (1..5).each do |n|
+          updates << get_unupdated_bin_track_indicators(bins, n)
+        end
+
+        Bin.update_all(ActiveRecord::Base.extend_set_sql_with_request(updates.compact.join(','),"bins"), "bins.delivery_id=#{@delivery.id}")
 
         Inventory.create_stock(@delivery.owner_party_role_id, "BIN", @delivery.farm_code, @delivery.truck_registration_number, "receive_intake_bins", @delivery.delivery_number, "INTAKE", bin_nums)
 
         delivery                 = Delivery.find_by_delivery_number(@delivery_number)
         delivery.delivery_status = "intake_bin_scan_completed"
         delivery.update
-  # m.bla
-        empty_units_quantity = delivery.quantity_empty_units
-        #if empty_units_quantity == nil ||   empty_units_quantity == 0
+
         DeliveryRouteStep.update_all(ActiveRecord::Base.extend_set_sql_with_request("date_completed = '#{Time.now()}' ","delivery_route_steps"), "delivery_route_steps.delivery_id = '#{delivery.id}' and delivery_route_steps.route_step_code = 'intake_bin_scanning'")
-        #end
 
         self.set_transaction_complete_flag
         result        = ["Bin scan completed successfully "]
@@ -255,6 +257,12 @@ puts
     rescue
       DeliveryRouteStep.update_all(ActiveRecord::Base.extend_set_sql_with_request("date_activated = null","delivery_route_steps"), "delivery_route_steps.delivery_id = '#{@delivery_id}' and delivery_route_steps.route_step_code = 'intake_bin_scanning'")
       raise $!
+    end
+  end
+
+  def get_unupdated_bin_track_indicators(bins, n)
+    if ((failed=bins.find { |d| !eval("d.track_indicator#{n}_id") }) && (succ=bins.find { |d| eval("d.track_indicator#{n}_id") }))
+      return "track_indicator#{n}_id = #{eval("succ.track_indicator#{n}_id")} "
     end
   end
 end
