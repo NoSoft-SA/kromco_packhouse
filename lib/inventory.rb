0324105857a60dc5aff870859b628303ed1523bd
patie <patie@nosoft.biz>
2019-11-05 09:38:33 +0200

    bin_putaway_plan #10

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -548,7 +548,7 @@ module Inventory
 
       location_from.units_in_location = location_from.units_in_location.to_i - 1
       location_from.updated_at = Time.now
-      location_from.updated_by = ActiveRequest.get_active_request.user
+      #location_from.updated_by = ActiveRequest.get_active_request.user
       location_from.update #:TODO this was commented out any particular reason
 
       stock_locations_history.units_in_location_after = location_from.units_in_location
@@ -562,7 +562,7 @@ module Inventory
 
       location_to = Location.find_by_location_code(@stock_item_after.location_code)
       location_to.updated_at = Time.now
-      location_to.updated_by = ActiveRequest.get_active_request.user
+      #location_to.updated_by = ActiveRequest.get_active_request.user
       location_to.update
       #======== Log stock_locations_history=================================
       stock_locations_history = StockLocationsHistory.new({:inventory_transaction_id => @inventory_transaction.id, :stock_item_id => @stock_item_after.id, :inventory_reference => @stock_item_after.inventory_reference,
