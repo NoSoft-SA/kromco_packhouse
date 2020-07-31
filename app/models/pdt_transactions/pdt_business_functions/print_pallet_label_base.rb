e97c6f0ebbcd7b2c12a630453ac09ae459a58d46
patie <patie@nosoft.biz>
2020-07-31 12:49:40 +0200

    remove auto submit and add a button

--- app/models/pdt_transactions/pdt_business_functions/print_pallet_label_base.rb
+++ app/models/pdt_transactions/pdt_business_functions/print_pallet_label_base.rb
@@ -15,7 +15,7 @@ class PrintPalletLabelBase < PDTTransaction
     field_configs[field_configs.length] = {:type=>"text_box", :name=>"printer", :label=>"scan printer", :is_required=>"true"}
     #field_configs[field_configs.length] = {:type=>"text_box", :name=>"n_labels_to_print", :label=>"print qty"}
     field_configs[field_configs.length] = {:type=>"drop_down", :name=>"n_labels_to_print", :label=>"print qty", :list=>"1,2,3,4"}
-    field_configs[field_configs.length] = {:type=>"text_box", :name=>"carton_or_pallet", :label=>"scan ctn or plt", :is_required=>"true",:scan_field => true,:submit_form => true}
+    field_configs[field_configs.length] = {:type=>"text_box", :name=>"carton_or_pallet", :label=>"scan ctn or plt", :is_required=>"true",:scan_field => true}
 
     screen_attributes                   = {:auto_submit=>"false", :content_header_caption=>"scan carton or pallet and printer"}
     buttons                             = {"B3Label"=>"Clear" ,"B2Label"=>"", "B2Submit"=>"", "B1Submit"=>"print_pallet_label_base_submit","B1Label"=>"submit","B1Enable"=>"true","B2Enable"=>"false","B3Enable"=>"false" }
